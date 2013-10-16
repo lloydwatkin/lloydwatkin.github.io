a99da44f1fe1f13cfb9d2aad44f75a512cc52268
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-10-16 19:22:40 +0100

    Fall back to usb0 connection if my wifis are messing up

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@
 # Start jekyll with local IP address for demos/workshops
 
 export MY_IP=$(ifconfig | grep -A1 wlan0 | cut -d: -f2 | cut -d ' ' -f1 | grep \\.)
+if [ "" == "$MY_IP" ]; then 
+  export MY_IP=$(ifconfig | grep -A1 usb0 | cut -d: -f2 | cut -d ' ' -f1 | grep \\.);
+fi
 export JEKYLL_ENV=development
 export XMPP_FTW_PORT=3000
 echo "My IP is $MY_IP"
